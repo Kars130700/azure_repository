412d80a2aa98ffc9a49a0ae1bd678f4b7cd96925
Kars130700 <39276875+Kars130700@users.noreply.github.com>
2024-02-06 18:27:27 +0000

    redid everything should work

--- azure-upload-file-to-storage/app/src/App.tsx
+++ azure-upload-file-to-storage/app/src/App.tsx
@@ -1,8 +1,7 @@
 import { BlockBlobClient } from '@azure/storage-blob';
 import { Box, Card, Button, CardMedia, Grid, Typography } from '@mui/material';
-//import { styled } from '@mui/material/styles';
-//import CloudUploadIcon from '@mui/icons-material/CloudUpload';
-import { useState } from 'react';
+// import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import { ChangeEvent, useState } from 'react';
 import ErrorBoundary from './components/error-boundary';
 import NavBar from './components/navbar';
 import { convertFileToArrayBuffer } from './lib/convert-file-to-arraybuffer';
@@ -34,23 +33,20 @@ function App() {
   const [uploadStatus, setUploadStatus] = useState<string>('');
   const [list, setList] = useState<string[]>([]);
 
-  const handleFilesAccepted = (files : File[]) => {
-    setSelectedFiles(files);
-  };
-  // const handleFileSelection = (event: ChangeEvent<HTMLInputElement>) => {
-  //   const { target } = event;
-    
-  //   if (!(target instanceof HTMLInputElement)) return;
-  //   if (!target.files || target.files.length === 0) return;
+  const handleFileSelection = (event: ChangeEvent<HTMLInputElement>) => {
+    const { target } = event;
   
-  //   // Convert FileList to array and update state
-  //   const filesArray = Array.from(target.files);
-  //   setSelectedFiles(filesArray);
-  //   console.log(selectedFiles);
-  //   // Reset other states
-  //   setSasTokenUrl('');
-  //   setUploadStatus('');
-  // };
+    if (!(target instanceof HTMLInputElement)) return;
+    if (!target.files || target.files.length === 0) return;
+  
+    // Convert FileList to array and update state
+    const filesArray = Array.from(target.files);
+    setSelectedFiles(filesArray);
+  
+    // Reset other states
+    setSasTokenUrl('');
+    setUploadStatus('');
+  };
 
   const handleFileSasToken = () => {
     const permission = 'w'; //write
@@ -168,7 +164,7 @@ function App() {
                 <div className='box-label'>
                 <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
                 <label className="form-check-label" htmlFor="flexCheckDefault">
-                  Default checkbox
+                  Cooking time per year
                 </label>
                 </div>
                 <div className='box-label'>
@@ -200,9 +196,10 @@ function App() {
               </div>
               <div className='filler'></div>
               <div className='upload-button-div'>
-                <Button component="label" variant="contained" onClick={handleFileUpload}>
+                <Button component="label" color='secondary' variant="contained"  onClick={handleFileUpload}>
                   Upload
                 </Button>
+                <a type="button" className="btn btn-purple"><i className="fas fa-heart pr-2" aria-hidden="false"></i>Heart</a>
               </div>
           </div>
           {/* App Title */}
@@ -226,7 +223,7 @@ function App() {
           >
             <Button variant="contained" component="label">
              Select Files
-            <input type="file" hidden multiple onChange={handleFileUpload} />
+            <input type="file" hidden multiple onChange={handleFileSelection} />
             </Button>
             {selectedFiles.length > 0 && (
               <Box my={2}>
