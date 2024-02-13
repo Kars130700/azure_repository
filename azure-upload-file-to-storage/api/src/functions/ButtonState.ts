86ca3ae84f4d74891a20157351e46c168490f40d
Kars130700 <39276875+Kars130700@users.noreply.github.com>
2024-02-13 10:25:02 +0000

    nog maar eens, nu met expliciet toevoegen van url

--- azure-upload-file-to-storage/api/src/functions/ButtonState.ts
+++ azure-upload-file-to-storage/api/src/functions/ButtonState.ts
@@ -4,28 +4,49 @@ import {
   HttpResponseInit,
   InvocationContext
 } from '@azure/functions';
+import * as http from 'http';
 
 export async function ButtonState(
   request: HttpRequest,
   context: InvocationContext
 ): Promise<HttpResponseInit> {
-  console.log('Hello!');
-  context.log(`Http function processed request for url "${request.url}"`);
+  if (request.method == 'POST') {
+    const aggregatedCheckedValue =
+      request.query.get('aggregatedChecked') || 'false';
+    const postUrl =
+      'https://thankful-sky-09024e01e.4.azurestaticapps.net/api/ButtonState';
+    // Create the HTTP request options
+    const options: http.RequestOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
 
-  const aggregatedCheckedValue =
-    request.query.get('aggregatedChecked') || 'false';
+    // Create a request object
+    const httpRequest = http.request(postUrl, options, (response) => {
+      // Handle the response from the server
+      if (response.statusCode === 200) {
+        // Respond with a success message
+        context.log('Button state posted successfully');
+      } else {
+        // Respond with an error message
+        context.error;
+      }
+    });
 
-  context.log(`Aggregated Checked Value: ${aggregatedCheckedValue}`);
-  // Log the value to the console
-  console.log(`Aggregated Checked Value: ${aggregatedCheckedValue}`);
+    // Write the data to the request body
+    httpRequest.write(JSON.stringify({ aggregatedCheckedValue }));
 
-  // Return the value in the HTTP response
-  return { body: aggregatedCheckedValue };
+    // End the request
+    httpRequest.end();
+    return { body: aggregatedCheckedValue };
+  }
 }
 
 //configures the azure functions to handle HTTP request under route ButtonState
 app.http('ButtonState', {
-  methods: ['POST'],
+  methods: ['POST', 'GET'],
   authLevel: 'anonymous',
   handler: ButtonState
 });
