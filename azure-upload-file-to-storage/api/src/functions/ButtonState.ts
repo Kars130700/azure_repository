831cb5d5440ab902760eaf8061f17f0ba243c815
Kars130700 <39276875+Kars130700@users.noreply.github.com>
2024-02-13 10:45:49 +0000

    removed unneccesary info buttonstate

--- azure-upload-file-to-storage/api/src/functions/ButtonState.ts
+++ azure-upload-file-to-storage/api/src/functions/ButtonState.ts
@@ -13,16 +13,13 @@ export async function ButtonState(
 ): Promise<HttpResponseInit> {
   context.log(`Http function processed request for url "${request.url}"`);
 
-  // Get the name parameter from the query string
-  const name = request.query.get('name') || 'world';
-
   // Get the aggregatedChecked parameter from the query string
   const aggregatedCheckedParam = request.query.get('aggregatedChecked');
   if (aggregatedCheckedParam) {
     aggregatedChecked = aggregatedCheckedParam.toLowerCase() === 'true';
   }
 
-  return { body: `Hello, ${name}! AggregatedChecked: ${aggregatedChecked}` };
+  return { body: `${aggregatedChecked}` };
 }
 
 app.http('ButtonState', {
