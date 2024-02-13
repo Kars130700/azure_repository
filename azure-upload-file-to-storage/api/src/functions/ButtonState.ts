23d46d90d56d67d99fef22c2c40f69aa04367c72
Kars130700 <39276875+Kars130700@users.noreply.github.com>
2024-02-13 11:12:25 +0000

    added boolean string

--- azure-upload-file-to-storage/api/src/functions/ButtonState.ts
+++ azure-upload-file-to-storage/api/src/functions/ButtonState.ts
@@ -5,8 +5,6 @@ import {
   InvocationContext
 } from '@azure/functions';
 
-let aggregatedChecked = false;
-
 export async function ButtonState(
   request: HttpRequest,
   context: InvocationContext
@@ -14,12 +12,28 @@ export async function ButtonState(
   context.log(`Http function processed request for url "${request.url}"`);
 
   // Get the aggregatedChecked parameter from the query string
-  const aggregatedCheckedParam = request.query.get('aggregatedChecked');
-  if (aggregatedCheckedParam) {
-    aggregatedChecked = aggregatedCheckedParam.toLowerCase() === 'true';
-  }
+  const aggregatedChecked = request.query.get('aggregatedChecked');
+  const yearlyChecked = request.query.get('yearlyChecked');
+  const monthlyChecked = request.query.get('monthlyChecked');
+  const dailyChecked = request.query.get('dailyChecked');
+  const PDFChecked = request.query.get('PDFChecked');
+  const ExcelChecked = request.query.get('ExcelChecked');
 
-  return { body: `${aggregatedChecked}` };
+  const checkboxNames = [
+    aggregatedChecked,
+    yearlyChecked,
+    monthlyChecked,
+    dailyChecked,
+    PDFChecked,
+    ExcelChecked
+  ];
+
+  // Use Array.map to convert each checkbox value to '1' if true, '0' if false
+  const resultBinaryString = checkboxNames
+    .map((checkboxName) => (request.query.get(checkboxName) ? '1' : '0'))
+    .join('');
+
+  return { body: `${resultBinaryString}` };
 }
 
 app.http('ButtonState', {
