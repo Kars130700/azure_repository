d24cf8ab0d447c578ef63e8b4f43ef3a3c240afe
Kars130700 <39276875+Kars130700@users.noreply.github.com>
2024-04-17 14:08:49 +0000

    adds tabledata correctly

--- azure-upload-file-to-storage/app/src/components/downloadTable.tsx
+++ azure-upload-file-to-storage/app/src/components/downloadTable.tsx
@@ -221,12 +221,9 @@ export default function DownloadTable( { tableData }: { tableData: Data[] },  ) 
   const [selected, setSelected] = React.useState<readonly number[]>([]);
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
-  const rows = React.useMemo(
-    () =>
-      tableData.map(data => createData(data.id, data.fileName, data.uploaderName, data.date, data.url)),
-    [tableData]
-  );
-
+  const rows = tableData.map(data => createData(data.id, data.fileName, data.uploaderName, data.date, data.url));
+  console.log("rows:")
+  console.log(rows)
   const handleRequestSort = (
     event: React.MouseEvent<unknown>,
     property: keyof Data,
@@ -278,7 +275,7 @@ export default function DownloadTable( { tableData }: { tableData: Data[] },  ) 
         page * rowsPerPage,
         page * rowsPerPage + rowsPerPage,
       ),
-    [order, orderBy, page, rowsPerPage],
+    [order, orderBy, page, rowsPerPage, rows],
   );
 
   const downloadFile = (fileUrl: string, fileName: string) => {
@@ -287,7 +284,6 @@ export default function DownloadTable( { tableData }: { tableData: Data[] },  ) 
     })
     .then(response => {
         const url = window.URL.createObjectURL(new Blob([response.data]));
-        console.log('print');
         console.log(fileUrl)
         const link = document.createElement('a');
         link.href = url;
